a0724283bc5f827b16f710b884dfc97e76a0b2a3
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-07 13:08:21 -0700

    create schema for cakes

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS cakes_db;
+
+CREATE DATABASE cakes_db;
+
+USE cakes_db;
+
+CREATE TABLE cake (
+    id INT NOT NULL AUTO_INCREMENT,
+    cake_name VARCHAR(100),
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
+)
