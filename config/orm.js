5e77e844119ab5bd4cf793eed4bc629e0a84ad71
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-08 16:37:22 -0700

    pseudo code for orm

---
+++ config/orm.js
@@ -0,0 +1,25 @@
+// dependencies
+var connection = require("./connection.js");
+
+// create orm
+var orm = {
+    // selectAll()
+    selectAll: function() {
+
+    },
+    // insertOne()
+    insertOne: function() {
+
+    },
+    // updateOne()
+    updateOne: function() {
+
+    },
+    // deleteOne()
+    deleteOne: function() {
+
+    }
+};
+
+// export!
+module.exports = orm;
