5db99e73f9575a8bcbe975f6f521dd692dc3ae88
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-08 17:02:08 -0700

    add logic to cake.all() and more pseudocode

--- models/cake.js
+++ models/cake.js
@@ -3,12 +3,21 @@ var orm = require("../config/orm.js");
 
 var cake = {
     // all!
-
+    all: function(cb) {
+        // use orm.selectAll(table, cb)
+        orm.selectAll("cakes", function(res) {
+            // callback value
+            cb(res)
+        })
+    }
     // insert!
+        // use orm.insertOne(table, name, bool, cb)
 
     // update!
+        // use orm.updateOne(table, val, condition, cb)
 
     // delete!
+        // use orm.deleteOne(table, condition, cb)
 }
 
 // export!
