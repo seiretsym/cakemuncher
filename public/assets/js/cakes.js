7274c739beaabd2de86a2e6f8a60cdbdbcd35717
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-08 19:41:57 -0700

    add id to textarea // logic to onClick listeners

--- public/assets/js/cakes.js
+++ public/assets/js/cakes.js
@@ -5,6 +5,14 @@ $(function() {
         event.preventDefault();
 
         // post!
+        $.ajax({
+            url: "/api/cakes",
+            type: "POST",
+            data: { cake_name: $("#cake").val().trim() }
+        }).then(function() {
+            // reload page
+            location.reload();
+        })
     });
 
     // on click listener for eat
@@ -13,6 +21,13 @@ $(function() {
         event.preventDefault();
         
         // put!
+        $.ajax({
+            url: "/api/cakes" + $(this).data("id"),
+            type: "PUT"
+        }).then(function() {
+            // reload page
+            location.reload();
+        })
     });
 
     // on click listener for delete
@@ -21,6 +36,12 @@ $(function() {
         event.preventDefault();
 
         // delete!
-
+        $.ajax({
+            url: "/api/cakes" + $(this).data("id"),
+            type: "DELETE"
+        }).then(function() {
+            // reload page
+            location.reload();
+        })
     });
 })
