fa2a1a6c947c65a04ed301bf1588dce5c80b29db
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-08 19:36:36 -0700

    button click listeners fixed!

--- public/assets/js/cakes.js
+++ public/assets/js/cakes.js
@@ -1,24 +1,26 @@
-// on click listener for add
-$(".add").on("click", function(event) {
-    event.preventDefault();
+$(function() {
+    // on click listener for add
+    $("#add").on("click", function(event) {
+        // prevent button from doing normal stuff
+        event.preventDefault();
 
-    console.log("Add button clicked!")
-    // post!
-});
+        // post!
+    });
 
-// on click listener for eat
-$("#eat").on("click", function(event) {
-    event.preventDefault();
-    
-    console.log("Eat button clicked!")
-    // put!
-}
+    // on click listener for eat
+    $(".eat").on("click", function(event) {
+        // prevent button from doing normal stuff
+        event.preventDefault();
+        
+        // put!
+    });
 
-// on click listener for delete
-$(".delete").on("click", function(event) {
-    event.preventDefault();
-    console.log("Delete button clicked!")
+    // on click listener for delete
+    $(".delete").on("click", function(event) {
+        // prevent button from doing normal stuff
+        event.preventDefault();
 
-    // delete!
+        // delete!
 
+    });
 })
