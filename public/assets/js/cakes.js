08c919c174dc50ca2eefda0f2050b04ec4091f5e
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-08 19:24:13 -0700

    add onClick listeners and some pseudocode

---
+++ public/assets/js/cakes.js
@@ -0,0 +1,24 @@
+// on click listener for add
+$(".add").on("click", function(event) {
+    event.preventDefault();
+
+    console.log("Add button clicked!")
+    // post!
+});
+
+// on click listener for eat
+$(".eat").on("click", function(event) {
+    event.preventDefault();
+    
+    console.log("Eat button clicked!")
+    // put!
+}
+
+// on click listener for delete
+$(".delete").on("click", function(event) {
+    event.preventDefault();
+    console.log("Delete button clicked!")
+
+    // delete!
+
+})
