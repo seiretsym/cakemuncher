7a8eb6571a96c0f222b8f547fb566643f8cfb6c7
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-08 17:14:29 -0700

    add more pseudocoding

--- controllers/cakes_controller.js
+++ controllers/cakes_controller.js
@@ -7,21 +7,27 @@ var cakes = express.Router();
 
 // get
 cakes.get("/", function(req, res) {
-
+    // use cake.all(cb)
+    cake.all(function(data) {
+        // get back to this after you create handlebars
+    })
 });
 
 // insert
 cakes.post("/api/cakes", function(req, res) {
+    // use cake.insert(name, bool, cb)
 
 });
 
 // update
 cakes.put("/api/cakes/:id", function(req, res) {
+    // use cake.update(val, condition, cb)
 
 });
 
 // delete
 cakes.delete("/api/cakes/:id", function(req, res) {
+    // use cake.delete(condition, cb)
 
 });
 
