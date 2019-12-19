2062961ee20058fc5e0e13e3a9f2ece203358883
Kerwin Hy <kerwin.hy@gmail.com>
2019-12-19 10:38:38 -0800

    change connection settings for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,18 @@
 var mysql = require("mysql");
 
 var pool = mysql.createPool({
-    host: "localhost",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306,
-    user: "root",
-    password: "derp",
+    user: "b557827e8ecf47",
+    password: "0942987c",
     database: "cakes_db"
 })
 
-pool.on("acquire", function(connection) {
+pool.on("acquire", function (connection) {
     console.log("Connected as Derp #" + connection.threadId);
 });
 
-pool.on("release", function(connection) {
+pool.on("release", function (connection) {
     console.log("Connection released from Derp #" + connection.threadId);
 });
 
